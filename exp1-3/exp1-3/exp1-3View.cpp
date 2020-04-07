0113b23fb8bcfe3ff35eade1d7fd14cf4f440f71
cf-GitHub-114 <1061737971@qq.com>
2020-04-07 20:36:13 +0800

    对View和Doc类中的两个字符串赋初值，左键鼠标，两个字符串拼接，并在客户区显示。

---
+++ exp1-3/exp1-3/exp1-3View.cpp
@@ -0,0 +1,81 @@
+
+// exp1-3View.cpp : Cexp13View 类的实现
+//
+
+#include "stdafx.h"
+// SHARED_HANDLERS 可以在实现预览、缩略图和搜索筛选器句柄的
+// ATL 项目中进行定义，并允许与该项目共享文档代码。
+#ifndef SHARED_HANDLERS
+#include "exp1-3.h"
+#endif
+
+#include "exp1-3Doc.h"
+#include "exp1-3View.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// Cexp13View
+
+IMPLEMENT_DYNCREATE(Cexp13View, CView)
+
+BEGIN_MESSAGE_MAP(Cexp13View, CView)
+END_MESSAGE_MAP()
+
+// Cexp13View 构造/析构
+
+Cexp13View::Cexp13View()
+{
+	// TODO: 在此处添加构造代码
+
+}
+
+Cexp13View::~Cexp13View()
+{
+}
+
+BOOL Cexp13View::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO: 在此处通过修改
+	//  CREATESTRUCT cs 来修改窗口类或样式
+
+	return CView::PreCreateWindow(cs);
+}
+
+// Cexp13View 绘制
+
+void Cexp13View::OnDraw(CDC* /*pDC*/)
+{
+	Cexp13Doc* pDoc = GetDocument();
+	ASSERT_VALID(pDoc);
+	if (!pDoc)
+		return;
+
+	// TODO: 在此处为本机数据添加绘制代码
+}
+
+
+// Cexp13View 诊断
+
+#ifdef _DEBUG
+void Cexp13View::AssertValid() const
+{
+	CView::AssertValid();
+}
+
+void Cexp13View::Dump(CDumpContext& dc) const
+{
+	CView::Dump(dc);
+}
+
+Cexp13Doc* Cexp13View::GetDocument() const // 非调试版本是内联的
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(Cexp13Doc)));
+	return (Cexp13Doc*)m_pDocument;
+}
+#endif //_DEBUG
+
+
+// Cexp13View 消息处理程序
---
+++ exp1-3/exp1-3/exp1-3View.h
@@ -0,0 +1,46 @@
+
+// exp1-3View.h : Cexp13View 类的接口
+//
+
+#pragma once
+
+
+class Cexp13View : public CView
+{
+protected: // 仅从序列化创建
+	Cexp13View();
+	DECLARE_DYNCREATE(Cexp13View)
+
+// 特性
+public:
+	Cexp13Doc* GetDocument() const;
+
+// 操作
+public:
+
+// 重写
+public:
+	virtual void OnDraw(CDC* pDC);  // 重写以绘制该视图
+	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+protected:
+
+// 实现
+public:
+	virtual ~Cexp13View();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+protected:
+
+// 生成的消息映射函数
+protected:
+	DECLARE_MESSAGE_MAP()
+};
+
+#ifndef _DEBUG  // exp1-3View.cpp 中的调试版本
+inline Cexp13Doc* Cexp13View::GetDocument() const
+   { return reinterpret_cast<Cexp13Doc*>(m_pDocument); }
+#endif
+
