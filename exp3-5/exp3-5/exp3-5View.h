66ebaab0ad625ac56a0ac0882813615a09876c9a
cf-GitHub-114 <1061737971@qq.com>
2020-04-21 21:54:10 +0800

    新建

---
+++ exp3-5/exp3-5/exp3-5View.h
@@ -0,0 +1,46 @@
+
+// exp3-5View.h : Cexp35View 类的接口
+//
+
+#pragma once
+
+
+class Cexp35View : public CView
+{
+protected: // 仅从序列化创建
+	Cexp35View();
+	DECLARE_DYNCREATE(Cexp35View)
+
+// 特性
+public:
+	Cexp35Doc* GetDocument() const;
+
+// 操作
+public:
+
+// 重写
+public:
+	virtual void OnDraw(CDC* pDC);  // 重写以绘制该视图
+	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+protected:
+
+// 实现
+public:
+	virtual ~Cexp35View();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+protected:
+
+// 生成的消息映射函数
+protected:
+	DECLARE_MESSAGE_MAP()
+};
+
+#ifndef _DEBUG  // exp3-5View.cpp 中的调试版本
+inline Cexp35Doc* Cexp35View::GetDocument() const
+   { return reinterpret_cast<Cexp35Doc*>(m_pDocument); }
+#endif
+
