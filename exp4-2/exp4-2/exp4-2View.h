c991e9765b97ec4322c1c3fd3e9e33fbe27bc200
cf-GitHub-114 <1061737971@qq.com>
2020-04-28 19:35:33 +0800

    新建

---
+++ exp4-2/exp4-2/exp4-2View.h
@@ -0,0 +1,46 @@
+
+// exp4-2View.h : Cexp42View 类的接口
+//
+
+#pragma once
+
+
+class Cexp42View : public CView
+{
+protected: // 仅从序列化创建
+	Cexp42View();
+	DECLARE_DYNCREATE(Cexp42View)
+
+// 特性
+public:
+	Cexp42Doc* GetDocument() const;
+
+// 操作
+public:
+
+// 重写
+public:
+	virtual void OnDraw(CDC* pDC);  // 重写以绘制该视图
+	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+protected:
+
+// 实现
+public:
+	virtual ~Cexp42View();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+protected:
+
+// 生成的消息映射函数
+protected:
+	DECLARE_MESSAGE_MAP()
+};
+
+#ifndef _DEBUG  // exp4-2View.cpp 中的调试版本
+inline Cexp42Doc* Cexp42View::GetDocument() const
+   { return reinterpret_cast<Cexp42Doc*>(m_pDocument); }
+#endif
+
