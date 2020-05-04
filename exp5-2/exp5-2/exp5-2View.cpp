ee4d8d84f0940dd46fd7ec77ffa07edf3a0fdaf0
cf-GitHub-114 <1061737971@qq.com>
2020-05-04 12:18:54 +0800

    添加菜单，鼠标左键单击该菜单后, 在客户区画出以客户区中点为圆心、半径由小到大渐变的圆,颜色也渐变

--- exp5-2/exp5-2/exp5-2View.cpp
+++ exp5-2/exp5-2/exp5-2View.cpp
@@ -22,6 +22,7 @@
 IMPLEMENT_DYNCREATE(Cexp52View, CView)
 
 BEGIN_MESSAGE_MAP(Cexp52View, CView)
+	ON_COMMAND(ID_VIEW_SHOWCOLORELLIPSE, &Cexp52View::OnViewShowcolorellipse)
 END_MESSAGE_MAP()
 
 // Cexp52View 构造/析构
@@ -79,3 +80,29 @@ Cexp52Doc* Cexp52View::GetDocument() const // 非调试版本是内联的
 
 
 // Cexp52View 消息处理程序
+
+
+void Cexp52View::OnViewShowcolorellipse()
+{
+	// TODO: 在此添加命令处理程序代码
+	int red = 0, green = 255, blue = 0;
+	int width = 2;
+	CClientDC dc(this);//定义一个CClientDC的对象dc
+	CRect rc;//定义一个矩形对象
+	GetClientRect(&rc);//获取客户区大小
+	int r = 100;//定义一个整型变量r,为圆形的半径
+	int x = (rc.left + rc.right) / 2;//获取客户区中点
+	int y = (rc.top + rc.bottom) / 2;
+	for (int i = 0;i < 10;i++) {
+		int color = RGB(red,green,blue);//构造颜色
+		CPen newPen(PS_SOLID,width,color);//构造新笔
+		CPen *oldPen = dc.SelectObject(&newPen);//将新构造的笔和设备环境类联系起来
+		r -= 10;//先画大的圆，若先画小的圆，会被后面的大圆挡住
+		CRect A(x - r, y - r, x + r, y + r);
+		dc.Ellipse(A);
+		dc.SelectObject(oldPen);//将旧笔选入设备环境
+		red += 20;
+		green  -= 20;
+		blue += 20;
+	}
+}
