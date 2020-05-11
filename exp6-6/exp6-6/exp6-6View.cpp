8b2f4751a68fee715247e895f6c8488416699043
cf-GitHub-114 <1061737971@qq.com>
2020-05-11 13:41:02 +0800

    实现字符的插入功能：　以鼠标点击位置作为插入点，然后从键盘输入一个字符到字符串中

--- exp6-6/exp6-6/exp6-6View.cpp
+++ exp6-6/exp6-6/exp6-6View.cpp
@@ -22,6 +22,8 @@
 IMPLEMENT_DYNCREATE(Cexp66View, CView)
 
 BEGIN_MESSAGE_MAP(Cexp66View, CView)
+	ON_WM_CHAR()
+	ON_WM_LBUTTONDOWN()
 END_MESSAGE_MAP()
 
 // Cexp66View 构造/析构
@@ -29,7 +31,14 @@ END_MESSAGE_MAP()
 Cexp66View::Cexp66View()
 {
 	// TODO: 在此处添加构造代码
-
+	rect.left = 200;//初始化矩形
+	rect.top = 300;
+	rect.right = 800;
+	rect.bottom = 350;
+	s = "";//初始化字符串
+	flag = 0;//初始化左键点击的标志
+	count = 0;//初始化字符个数的变量
+	frontCount = 0;//初始化前半段字符个数的变量
 }
 
 Cexp66View::~Cexp66View()
@@ -46,7 +55,7 @@ BOOL Cexp66View::PreCreateWindow(CREATESTRUCT& cs)
 
 // Cexp66View 绘制
 
-void Cexp66View::OnDraw(CDC* /*pDC*/)
+void Cexp66View::OnDraw(CDC* pDC)
 {
 	Cexp66Doc* pDoc = GetDocument();
 	ASSERT_VALID(pDoc);
@@ -54,6 +63,7 @@ void Cexp66View::OnDraw(CDC* /*pDC*/)
 		return;
 
 	// TODO: 在此处为本机数据添加绘制代码
+	pDC->Rectangle(rect);//画矩形
 }
 
 
@@ -79,3 +89,43 @@ Cexp66Doc* Cexp66View::GetDocument() const // 非调试版本是内联的
 
 
 // Cexp66View 消息处理程序
+
+
+void Cexp66View::OnChar(UINT nChar, UINT nRepCnt, UINT nFlags)
+{
+	// TODO: 在此添加消息处理程序代码和/或调用默认值
+	CClientDC dc(this);
+	if(!flag){//鼠标未左键点击，显示输入字符形成字符串
+		count += 1;//字符个数加一
+		s = s + (char)nChar;
+	   size = dc.GetTextExtent(s);//获取字符串的像素长度
+	   if (size.cx < 599) {
+		   dc.TextOutW(201, 301, s);
+	   }
+	}
+	else {//鼠标左键点击，向字符串中插入一个字符
+		frontCount = count*(downX - 201) / size.cx;//求字符串前半段的字符个数count*((downX-201)/size.cx)
+		stringFront = s.Left(frontCount);//获取前半段字符串
+		/*CString ss;
+		ss.Format(_T("%d"), frontCount);
+		dc.TextOutW(0, 0, ss);//输出字符串前半段字符个数
+		dc.TextOutW(0, 20, stringFront);//输出字符串前半段字符
+		*/
+		//求字符串后半段的字符个数count-frontCount
+		stringBack = s.Right(count - frontCount);//获取后半段字符串
+		//dc.TextOutW(0, 40, stringBack);//输出字符串后半段字符
+		s = stringFront + (char)nChar + stringBack;
+		dc.TextOutW(201, 301, s);//将插入字符后的字符串输出
+	}
+	CView::OnChar(nChar, nRepCnt, nFlags);
+}
+
+
+void Cexp66View::OnLButtonDown(UINT nFlags, CPoint point)
+{
+	// TODO: 在此添加消息处理程序代码和/或调用默认值
+	CClientDC dc(this);
+	downX = point.x;//获取鼠标左键时的坐标
+	flag = 1;//标志鼠标左键按下
+	CView::OnLButtonDown(nFlags, point);
+}
--- exp6-6/exp6-6/exp6-6View.h
+++ exp6-6/exp6-6/exp6-6View.h
@@ -14,7 +14,16 @@ protected: // 仅从序列化创建
 // 特性
 public:
 	Cexp66Doc* GetDocument() const;
-
+	CRect rect;//定义一个矩形
+	CString s;//定义一个字符串
+	int downX;//定义鼠标按下的横坐标
+	int flag;//定义一个标志鼠标左键按下的标志
+	int count;//定义一个变量，装字符个数
+	int frontCount;//定义一个装字符串前半段字符个数的变量
+	//类CSize是对Windows结构SIZE的封装,结构SIZE表示一个矩形的长度和宽度
+	CSize size;//定义一个装字符串长度和宽度的结构
+	CString stringFront;//定义前半部分的字符串
+	CString stringBack;//定义后半部分的字符串
 // 操作
 public:
 
@@ -37,6 +46,9 @@ protected:
 // 生成的消息映射函数
 protected:
 	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnChar(UINT nChar, UINT nRepCnt, UINT nFlags);
+	afx_msg void OnLButtonDown(UINT nFlags, CPoint point);
 };
 
 #ifndef _DEBUG  // exp6-6View.cpp 中的调试版本
