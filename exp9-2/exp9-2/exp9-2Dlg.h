aae051967d69eb4c76bda79fde3f9845551bc2f6
cf-GitHub-114 <1061737971@qq.com>
2020-07-10 19:02:26 +0800

    新建

---
+++ exp9-2/exp9-2/exp9-2Dlg.h
@@ -0,0 +1,34 @@
+
+// exp9-2Dlg.h : 头文件
+//
+
+#pragma once
+
+
+// Cexp92Dlg 对话框
+class Cexp92Dlg : public CDialogEx
+{
+// 构造
+public:
+	Cexp92Dlg(CWnd* pParent = NULL);	// 标准构造函数
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_EXP92_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
